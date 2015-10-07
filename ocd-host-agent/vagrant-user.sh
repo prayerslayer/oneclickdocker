744961ad1e2601b4cd264096420ce98be8a97098
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-10-06 23:44:15 -0700

    install docker properly
    now starting a container also works

--- ocd-host-agent/vagrant-user.sh
+++ ocd-host-agent/vagrant-user.sh
@@ -7,9 +7,5 @@ sudo mv lein /usr/local/bin
 sudo chmod a+x /usr/local/bin/lein
 lein
 
-# enable remote api
 export DOCKER_HOST="tcp://0.0.0.0:4243"
-echo "" >> /etc/default/docker.io
-echo 'DOCKER_HOST="tcp://0.0.0.0:4243"' >> /etc/default/docker.io
-export DOCKER_OPTS="-s aufs -H tcp://0.0.0.0:4243 -d"
-service docker.io restart
+export DOCKER_OPTS="-s aufs -H tcp://0.0.0.0:4243 -D"
--- ocd-host-agent/vagrant.sh
+++ ocd-host-agent/vagrant.sh
@@ -3,7 +3,19 @@
 apt-get update
 
 # install docker
-apt-get install -y docker.io
+apt-get install curl
+curl -sSL https://get.docker.com/ | sh
+usermod -aG docker vagrant
+# enable remote api
+export DOCKER_HOST="tcp://0.0.0.0:4243"
+export DOCKER_OPTS="-s aufs -H tcp://0.0.0.0:4243 -d -D"
+echo "" >> /etc/default/docker
+echo 'DOCKER_HOST="tcp://0.0.0.0:4243"' >> /etc/default/docker
+echo "" >> /etc/default/docker
+echo 'DOCKER_OPTS="-s aufs -H tcp://0.0.0.0:4243 -D"' >> /etc/default/docker
+# kernel extras for device driver
+apt-get install "linux-image-extra-$(uname -r)"
+service docker restart
 
 # install java
 # TODO switch to java 8 as soon as it is in repo
