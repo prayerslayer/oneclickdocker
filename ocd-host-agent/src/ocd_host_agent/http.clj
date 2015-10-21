c267bab35ef60352ef07607f1e9255ce0ff992c7
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-10-21 23:47:09 +0200

    #3 implement start :godmode: stop :suspect: kill :finnadie:

--- ocd-host-agent/src/ocd_host_agent/http.clj
+++ ocd-host-agent/src/ocd_host_agent/http.clj
@@ -2,6 +2,7 @@
   (:require [clojure.tools.logging :as log]
             [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.util.response :as ring]
             [ocd-host-agent.docker-api :as docker])
   (:use ring.middleware.json
         ring.middleware.reload
@@ -31,7 +32,6 @@
               (map (fn [x]
                      (conj x (get-in thing x)))))))
 
-; TODO list port
 (def get-containers
   (GET "/containers"
     []
@@ -45,13 +45,13 @@
                                                       (:Image %))))
                                    first
                                    :RepoTags)
-                        :Port (get-in % [:HostConfig
-                                         :PortBindings
-                                         (keyword (str docker/DEFAULT_CONTAINER_PORT)
-                                                  "tcp")
-                                         0
-                                         :HostPort
-                                         (Integer/parseInt)])))
+                        :Port (-> (get-in % [:HostConfig
+                                             :PortBindings
+                                             (keyword (str docker/DEFAULT_CONTAINER_PORT)
+                                                      "tcp")
+                                             0
+                                             :HostPort])
+                                  (Integer/parseInt))))
          (map (partial keep-keys [:Id
                                   :Created
                                   :Name
@@ -60,8 +60,37 @@
                                   :Port
                                   :State
                                   [:Image :Id]
-                                  [:Image :Parent]]))
-         )))
+                                  [:Image :Parent]])))))
+
+(def alter-container-status
+  (POST "/containers"
+    {body :body}
+    (let [known? #{"start" "stop" "kill"}]
+      (if (or (not (known? (:command body)))
+              (nil? (:container body))
+              (nil? (:user body)))
+        {:status 400}
+        (let [cmd (:command body)
+              user (:user body)
+              container-id (:container body)
+              containers (->> (docker/list-containers)
+                              (filter #(= container-id (:Id %))))]
+          (if (empty? containers)
+            (ring/not-found (str "No container with id " container-id))
+            (let [container (docker/list-container (:Id (first containers)))]
+              (if (= user
+                     (get-in container [:Config :Labels :user]))
+                (case (.toLowerCase cmd)
+                  "start" (do
+                            (docker/start-container (first container))
+                            (ring/response nil))
+                  "stop" (do
+                           (docker/stop-container (first container))
+                           (ring/response nil))
+                  "kill" (do
+                           (docker/kill-container (first container))
+                           (ring/response nil)))
+                (ring/not-found "No such container of this user.")))))))))
 
 ; GET /containers ; for monitoring
 ;   >> id, port, user, status, image, health info von container (CPU/MEMORY/DISK)
@@ -71,10 +100,12 @@
 ;   >> start container x of user y
 
 (defroutes rest-api
-  get-containers)
+  get-containers
+  alter-container-status)
 
 (def main
   (-> rest-api
       (wrap-reload)
       ;(wrap-stacktrace)
+      (wrap-json-body {:keywords? true})
       (wrap-json-response)))
